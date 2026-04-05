3baadf7deb1cab0a99a607e3ec7a94b305f0d08d
ash-hashtag <hashtag438@gmail.com>
2026-04-05 17:48:29 +0530

    update dependencies

--- core/test_api.rs
+++
@@ -1,9 +0,0 @@
-use embed_anything::embeddings::embed::EmbedderBuilder;
-
-fn main() {
-    let _ = EmbedderBuilder::new()
-        .model_architecture("bert")
-        .onnx_model_path(Some("model.onnx"))
-        .tokenizer_path(Some("tokenizer.json"))
-        .from_local_onnx();
-}
